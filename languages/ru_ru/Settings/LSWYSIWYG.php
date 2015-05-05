69f617a40061bd44208108b67152c54a9d5cf4ec
Алексей <waw555@yandex.ru>
2015-05-05 15:19:06 +0500

    Add Russian Translations

---
+++ languages/ru_ru/Settings/LSWYSIWYG.php
@@ -0,0 +1,25 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+        // Basic Strings
+        'LBL_WYSIWYG_SETTINGS' => 'Конфигурация Визуального Редактора',
+        'LBL_WYSIWYG_CONFIG_DESCRIPTION' => 'Экспорт в таблицу данных из системы',
+	'LBL_MODULE_ON'=>'Вкл',
+	'LBL_MODULE_OFF'=>'Выкл',
+	'ACTIVATED'=>'Активирован',
+	'DEACTIVATED'=>'Деактивирован',
+);
+
+
+$jsLanguageStrings = array(
+        'JS_WYSIWYG_ACTIVATED' => 'Активирован',
+        'JS_WYSIWYG_DEACTIVATED' => 'Деактивирован',
+);
+
